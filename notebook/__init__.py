28ca673a8f403bdfd00bbfa56db8085a203cdbef
Ty Dunn <tydunn@umich.edu>
2019-04-11 11:10:03 +0000

    fixing init pylint errors

--- notebook/__init__.py
+++ notebook/__init__.py
@@ -4,10 +4,9 @@ Notebook package initializer.
 Ty Dunn <ty@tydunn.com>
 """
 import flask
-
-app = flask.Flask(__name__)
-
-app.config.from_object('notebook.config')
-
 import notebook.api
 import notebook.model
+
+APP = flask.Flask(__name__)
+
+APP.config.from_object('notebook.config')
